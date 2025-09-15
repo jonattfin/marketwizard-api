8331e208f3305f367d9340f6304035637cabedf9
John Finch <jonattfin@gmail.com>
2025-09-15 17:19:29 +0300

    added missing project

---
+++ MarketWizard.Auth/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using MarketWizard.Application.Contracts.Infra;
+
+
+namespace MarketWizard.User.Services;
+
+public class UserService() : IUserService
+{
+    public Guid GetAuthenticatedUserId()
+    {
+        // TODO - Implement this the right way
+        return Guid.Parse("62404bb7-35a3-45de-95af-e85ebebb1f6f");
+    }
+}
---
+++ MarketWizard.Auth/UserServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using MarketWizard.Application.Contracts.Infra;
+using MarketWizard.User.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarketWizard.User;
+
+public static class UserServiceRegistration
+{
+    public static IServiceCollection AddUserServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IUserService, UserService>();
+
+        return services;
+    }
+}
